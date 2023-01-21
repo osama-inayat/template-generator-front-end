6afaf1e6a1017bcc60ab46671e4c7930b8a5379b
osama-inayat <rajaosamainayat@gmail.com>
2023-01-22 00:30:49 +0500

    first-commit

---
+++ src/components/CreateTemplate.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      msg: "Osama inayat testing",
+    };
+  },
+  mounted() {
+    this.test();
+  },
+  methods: {
+    test() {
+      axios
+        .get("http://localhost:3000/greetings/hello")
+        .then((response) => {
+          this.msg = response.data.name;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+<style></style>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
+const proxy = require("http-proxy-middleware");
+
+module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+};
