5bf97acb4ea16446b650c7706dcb1487d23fa29b
osama-inayat <rajaosamainayat@gmail.com>
2023-01-28 05:42:32 +0500

    psuhed

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,9 +2,13 @@ import * as Types from "@/store/types";
 import axios from "axios";
 export default {
   [Types.CREATE_TEMPLATE]: (context, payload) => {
-    axios.post("/api/templates/", { template: payload }).then(({ data }) => {
-      context.commit(Types.SET_TEMPLATES, data);
-    });
+    axios
+      .post("https://plankton-app-datju.ondigitalocean.app/templates/", {
+        template: payload,
+      })
+      .then(({ data }) => {
+        context.commit(Types.SET_TEMPLATES, data);
+      });
   },
   [Types.UPDATE_TEMPLATE]: (context, payload) => {
     context.commit(Types.UPDATE_TEMPLATE, payload);
@@ -16,13 +20,17 @@ export default {
     });
   },
   [Types.FETCH_TEMPLATES]: (context) => {
-    axios.get("/api/templates/").then(({ data }) => {
-      context.commit(Types.SET_TEMPLATES, data);
-    });
+    axios
+      .get("https://plankton-app-datju.ondigitalocean.app/templates/")
+      .then(({ data }) => {
+        context.commit(Types.SET_TEMPLATES, data);
+      });
   },
   [Types.UPDATE_TEMPLATE]: (context, { id, payload }) => {
     axios
-      .put(`/api/templates/${id}`, { template: payload })
+      .put(`https://plankton-app-datju.ondigitalocean.app/templates/${id}`, {
+        template: payload,
+      })
       .then(({ data }) => {
         context.commit(Types.SET_TEMPLATES, data);
       });
